3d9c61cbec18eb7b99d5629db11cda9634e97dfb
Deny Chen <deny.chen@gmail.com>
2017-06-22 12:11:20 -0700

    Added second story to stories seed

--- seeders/20170621062223-stories.js
+++ seeders/20170621062223-stories.js
@@ -3,7 +3,8 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Stories', [
-      { id: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', title: 'Title', firstPageId: null }
+      { id: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', title: 'Story 1', firstPageId: null },
+      { id: 'd7482ea5-6800-4414-88bc-1c9651eedf0e', title: 'Story 2', firstPageId: null }
     ], {});
   },
 
