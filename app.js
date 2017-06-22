20cbce32f504f1757bee1acea73fa5a542043eee
Deny Chen <deny.chen@gmail.com>
2017-06-22 09:37:04 -0700

    Move endpoint to /api/*

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/stories', stories)
-app.use('/page', pages)
+app.use('/api', index);
+app.use('/api/users', users);
+app.use('/api/stories', stories)
+app.use('/api/page', pages)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
