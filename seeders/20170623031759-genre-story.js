1e604b44cc25cb07a3b85a357b22314af62795e3
Deny Chen <deny.chen@gmail.com>
2017-06-22 20:45:35 -0700

    Add GenreStory seed

--- seeders/20170623031759-genre-story.js
+++ seeders/20170623031759-genre-story.js
@@ -4,18 +4,12 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('GenreStories', [
       { genreId: '1', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef' },
-      { genreId: '1', storyId: 'd7482ea5-6800-4414-88bc-1c9651eedf0e' },
-      { genreId: '2', storyId: 'd7482ea5-6800-4414-88bc-1c9651eedf0e' }
+      { genreId: '2', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef' },
+      { genreId: '1', storyId: 'd7482ea5-6800-4414-88bc-1c9651eedf0e' }
     ], {});
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('GenreStories', null, {});
   }
 };
