42436c8c6541e8b4720f1eda09fb629a813e755d
Deny Chen <deny.chen@gmail.com>
2017-08-09 18:34:52 -0700

    Update storyError class name

--- errors/storyError.js
+++ errors/storyError.js
@@ -1,4 +1,4 @@
-module.exports = class AuthError extends require('./appError') {
+module.exports = class StoryError extends require('./appError') {
   constructor (message, status) {
     super(message || 'There was an error creating or updating the story', status || 401);
   }
