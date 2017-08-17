680804ef45e5a57f866b3c773bad55cd905e9e5a
Deny Chen <deny.chen@gmail.com>
2017-08-16 18:47:31 -0700

    Revert "Add compression"
    This reverts commit 15012347f517b775c610c2317952a7f053c33b6e.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var compression = require('compression');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -37,8 +36,6 @@ app.use(function (req, res, next) {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(compression());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
