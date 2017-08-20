312b129ee4a1b2e0976091c0fd0b7953bce8cded
Deny Chen <deny.chen@gmail.com>
2017-08-20 10:43:05 -0700

    Move DB password to environment variable

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ let sequelize;
 if (dbConfig.use_env_variable) {
   sequelize = new Sequelize(process.env[dbConfig.use_env_variable]);
 } else {
-  sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+  sequelize = new Sequelize(dbConfig.database, dbConfig.username, process.env.MYSQL_PASSWORD, dbConfig);
 }
 
 fs
