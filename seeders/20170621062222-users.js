6351969914ab6eb13c37c92e126aa1b4bec2efd9
Deny Chen <deny.chen@gmail.com>
2017-08-24 12:22:10 -0700

    Update seeds

--- seeders/20170621062222-users.js
+++ seeders/20170621062222-users.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Users', [
-      { id: '99c039b3-1755-40dd-b85b-4a101dd71dea', username: 'denychen', email: 'deny.chen@gmail.com', password: '$2a$10$Ng9jKBHU.ecbAwIFGSsKGeUEorimqBseCxMzamzFL8hQkt9yHNkna' },
+      { id: '99c039b3-1755-40dd-b85b-4a101dd71dea', username: 'denychen', email: 'deny.chen@gmail.com', password: '$2a$10$6V5DXAb0L/ysSXA7d3DceuSA7VrJYBl3TJryZgdhfPKzBRQyki9KW' },
     ], {});
   },
 
--- seeders/20170621062224-pages.js
+++ seeders/20170621062224-pages.js
@@ -3,10 +3,10 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Pages', [
-      { id: '1080f223-a9a0-482d-9a68-133998bef225', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', content: 'There is someone attacking you. What do you do?' },
-      { id: '4ad8876a-63d1-4293-a602-9ddd676bae48', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', content: 'The attacker pulls out a samurai sword.' },
-      { id: '8ba6dada-f3af-4633-9ea7-70607ce6e51d', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', content: 'The attacker approaches.' },
-      { id: '4ada7d0a-490e-4211-ab00-41d0d9a9edc2', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', content: 'You dead.' },
+      { id: '1080f223-a9a0-482d-9a68-133998bef225', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', name: 'First page', content: 'There is someone attacking you. What do you do?' },
+      { id: '4ad8876a-63d1-4293-a602-9ddd676bae48', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', name: 'Sword', content: 'The attacker pulls out a samurai sword.' },
+      { id: '8ba6dada-f3af-4633-9ea7-70607ce6e51d', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', name: 'Approach', content: 'The attacker approaches.' },
+      { id: '4ada7d0a-490e-4211-ab00-41d0d9a9edc2', storyId: '70d803bd-a47c-47fb-9c5a-2212ca08fdef', name: '', content: 'You dead.' },
     ], {});
   },
 
