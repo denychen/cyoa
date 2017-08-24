89934fc65e3bee700976264d2866c4bc43048236
Deny Chen <deny.chen@gmail.com>
2017-08-24 13:08:07 -0700

    Add console error logging

--- errors/appError.js
+++ errors/appError.js
@@ -13,7 +13,13 @@ module.exports = class AppError extends Error {
     this.name = this.constructor.name;
 
     Error.captureStackTrace(this, this.constructor);
+    this.logError(message);
 
     this.status = status || 500;
-  }
+  };
+
+  logError(message) {
+    let asterisks = Array(message.length + 19).fill('*').join("");
+    console.error(`\n${asterisks}\n***** ERROR: ${message} *****\n${asterisks}\n`);
+  };
 };
